dd33c1740a906ee993a8cb8e15f3ec42f1e57300
Aigul <46625461+Aigul9@users.noreply.github.com>
2024-12-01 22:55:10 +0300

    fix identation

--- year2024/Day_01/solution.sql
+++ year2024/Day_01/solution.sql
@@ -1,32 +1,32 @@
 with wish_list_expanded as (
-	select
-		child_id
-		, wishes ->> 'first_choice' as primary_wish
-		, wishes ->> 'second_choice' as backup_wish
-		, wishes -> 'colors' ->> 0 as favorite_color
-		, json_array_length(wishes -> 'colors') as color_count
-	from wish_lists
+    select
+        child_id
+        , wishes ->> 'first_choice' as primary_wish
+        , wishes ->> 'second_choice' as backup_wish
+        , wishes -> 'colors' ->> 0 as favorite_color
+        , json_array_length(wishes -> 'colors') as color_count
+    from wish_lists
 )
 select
-	c.name
-	, w.primary_wish
-	, w.backup_wish
-	, w.favorite_color
-	, w.color_count
-		, case t.difficulty_to_make
-		when 1 then 'Simple Gift'
-		when 2 then 'Moderate Gift'
-		else 'Complex Gift'
-	end as gift_complexity
-	, case t.category
-		when 'outdoor' then 'Outside Workshop'
-		when 'educational' then 'Learning Workshop'
-		else 'General Workshop'
-	end as workshop_assignment
+    c.name
+    , w.primary_wish
+    , w.backup_wish
+    , w.favorite_color
+    , w.color_count
+        , case t.difficulty_to_make
+        when 1 then 'Simple Gift'
+        when 2 then 'Moderate Gift'
+        else 'Complex Gift'
+    end as gift_complexity
+    , case t.category
+        when 'outdoor' then 'Outside Workshop'
+        when 'educational' then 'Learning Workshop'
+        else 'General Workshop'
+    end as workshop_assignment
 from children c
-	join wish_list_expanded w
-		on c.child_id = w.child_id
-	join toy_catalogue t
-		on w.primary_wish = t.toy_name
+    join wish_list_expanded w
+        on c.child_id = w.child_id
+    join toy_catalogue t
+        on w.primary_wish = t.toy_name
 order by c.name
 limit 5;
