0315977fa32ce798641d6106c589ff2aaf5b9899
Сибгатуллина Айгуль Ильдаровна <AIlSibgatullina@sberbank.ru>
2024-12-06 11:20:17 +0300

    fix indentation

--- year2024/day04/solution.sql
+++ year2024/day04/solution.sql
@@ -1,11 +1,11 @@
 with tags as (
-  select
-    toy_id,
-    toy_name,
-    array(select unnest(new_tags) except select unnest(previous_tags)) as added_tags,
-    array(select unnest(previous_tags) intersect select unnest(new_tags)) AS unchanged_tags,
-    array(select unnest(previous_tags) except select unnest(new_tags)) as removed_tags
-  from public.toy_production
+    select
+        toy_id,
+        toy_name,
+        array(select unnest(new_tags) except select unnest(previous_tags)) as added_tags,
+        array(select unnest(previous_tags) intersect select unnest(new_tags)) AS unchanged_tags,
+        array(select unnest(previous_tags) except select unnest(new_tags)) as removed_tags
+    from public.toy_production
 )
 select
     toy_id,
