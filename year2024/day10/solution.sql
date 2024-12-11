4096870347d1202a1e0dcab5c28b8e5c3ae6312c
Сибгатуллина Айгуль Ильдаровна <AIlSibgatullina@sberbank.ru>
2024-12-11 15:39:55 +0300

    update crosstab

--- year2024/day10/solution.sql
+++ year2024/day10/solution.sql
@@ -8,11 +8,6 @@ from crosstab(
         group by date, drink_name
         order by 1, 2
     $$,
-    $$
-        select distinct drink_name
-        from Drinks
-        where drink_name in ('Eggnog', 'Hot Cocoa', 'Peppermint Schnapps')
-        order by 1
-    $$
+    $$ values ('Eggnog'), ('Hot Cocoa'), ('Peppermint Schnapps') $$
 ) AS ct(date date, eggnog int, hot_cocoa int, peppermint_schnapps int)
 where eggnog = 198 and hot_cocoa = 38 and peppermint_schnapps = 298;
