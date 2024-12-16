efe26df3deabc2ca2209e092e951fc358c1d2555
Aigul9 <46625461+Aigul9@users.noreply.github.com>
2024-12-16 22:05:17 +0300

    fix indents

--- year2024/day16/solution.sql
+++ year2024/day16/solution.sql
@@ -1,14 +1,14 @@
 with prev_time as (
-	select
-	    place_name,
-		lead(timestamp) over(order by timestamp) - timestamp as interval
-	from areas
-		join sleigh_locations
-			on ST_Intersects(coordinate, polygon)
+    select
+        place_name,
+	lead(timestamp) over(order by timestamp) - timestamp as interval
+    from areas
+        join sleigh_locations
+            on ST_Intersects(coordinate, polygon)
 )
 select
-	place_name,
-	sum(interval) as total_hours_spent
+    place_name,
+    sum(interval) as total_hours_spent
 from prev_time
 group by place_name
 order by 2 desc nulls last;
