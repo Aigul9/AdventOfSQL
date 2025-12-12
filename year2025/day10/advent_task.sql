a5e27bd6641bd0560b489b0b26e89438b0e177fa
Aygul Sibgatullina <aygul.sibgatullina@lamoda.ru>
2025-12-12 13:46:51 +0300

    add insert examples

--- year2025/day10/advent_task.sql
+++ year2025/day10/advent_task.sql
@@ -19,3 +19,17 @@ CREATE TABLE wish_list (
    child_name  TEXT,
    raw_wish    TEXT
 );
+
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (1, 'James A.', ' BLUEY SUPERMARKET PLAY SET');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (2, 'Sade C.', 'lego star wars set ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (3, 'Juan Q.', '   SCOOTER ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (4, 'Samir S.', '   LEGO STAR WARS SET  ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (5, 'Priya E.', 'shaved ice machine   ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (6, 'Henry L.', '   mini brands fill the fridge');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (7, 'Ayumi C.', 'VR HEADSET');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (8, 'Juan Y.', 'BARBIE DREAMHOUSE   ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (9, 'Priya O.', '  VR HEADSET  ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (10, 'Lucia V.', ' LEGO CITY F1 CAR ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (11, 'Nadia M.', '  SCOOTER');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (12, 'Juan B.', 'Pokemon Trainer Box ');
+INSERT INTO wish_list (id, child_name, raw_wish) VALUES (13, 'Riley W.', ' heADphoNeS   ');
--- year2025/day11/advent_task.sql
+++ year2025/day11/advent_task.sql
@@ -21,3 +21,18 @@ INSERT INTO snowball_categories (id, official_category) VALUES
     (4, 'polar precision microball'),
     (5, 'everfrost training round'),
     (6, 'arctic blast premium');
+
+INSERT INTO snowball_inventory (id, batch_id, category_name, quantity, status) VALUES
+    (1, 'BATCH-35443-J', 'frost-flight deluxe', 19, NULL),
+    (2, 'BATCH-59767-M', 'frost-flight deluxe', 41, 'incomplete'),
+    (3, 'BATCH-44795-B', 'frost-flight deluxe', 21, 'ready'),
+    (4, 'BATCH-23396-C', 'north ridge compact', 0, 'incomplete'),
+    (5, 'BATCH-88907-A', 'frost-flight deluxe', -2, 'incomplete'),
+    (6, 'BATCH-42662-D', 'frost-flight deluxe', 47, 'needs review'),
+    (7, 'BATCH-37460-V', 'north ridge compact', 43, 'ready'),
+    (8, 'BATCH-21395-S', 'frost-flight deluxe', -2, 'ready'),
+    (9, 'BATCH-36100-E', 'frost-flight deluxe', 46, 'ready'),
+    (10, 'BATCH-64987-H', 'frost-flight deluxe', 43, NULL),
+    (11, 'BATCH-57576-Z', 'melty deluxe', -5, 'ready'),
+    (12, 'BATCH-56025-U', 'snowball v2', 11, 'ready'),
+    (13, 'BATCH-86556-W', 'snowball v2', 12, 'ready');
