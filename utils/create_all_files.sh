46fb135b588593186c1c29313ffeaa5dfd882d71
Aygul Sibgatullina <aygul.sibgatullina@lamoda.ru>
2025-12-15 17:04:07 +0300

    update utils

--- utils/create_all_files.sh
+++ utils/create_all_files.sh
@@ -10,6 +10,9 @@ for day in $(seq -w 01 24); do
 #    touch "$dir/test_data.sql"
     touch "$dir/solution.sql"
     touch "$dir/task.md"
+    touch "$dir/advent_task.md"
+    touch "$dir/advent_task.sql"
+    touch "$dir/advent_solution.sql"
 done
 
 echo "done"
